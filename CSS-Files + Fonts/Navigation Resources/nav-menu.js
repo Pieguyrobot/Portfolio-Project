6d9ea767ee76023afb309b889f88b08263607929
Pieguyrobot <107954440+Pieguyrobot@users.noreply.github.com>
2022-09-27 02:34:46 +0000

    Fixed Sidebar, Used to adjust weirdly.

--- CSS-Files + Fonts/Navigation Resources/nav-menu.js
+++ CSS-Files + Fonts/Navigation Resources/nav-menu.js
@@ -2,12 +2,12 @@ var toggle = 1;
 
 $(".nav-button").click(function () {
   if (toggle == 1) {
-    $(".nav-bar").css("width", "275px");
+    $(".nav-bar").css("marginLeft", "0px");
     $(".nav-bar a").css("marginLeft", "5px")
     toggle = 0;
   } 
   else if (toggle == 0) {
-    $(".nav-bar").css("width", "0px");
+    $(".nav-bar").css("marginLeft", "-275px");
     $(".nav-bar a").css("marginLeft", "-275px");
     toggle = 1;
   }
